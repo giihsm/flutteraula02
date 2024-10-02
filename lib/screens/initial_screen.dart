7127a1b3115e6e2ce610d5d8168c8bca17c2cf1e
Cristina Jung <cristijung@gmail.com>
2024-10-02 15:16:48 -0300

    componentes

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InitialScreen extends StatefulWidget {
+  const InitialScreen({super.key});
+
+  @override
+  State<InitialScreen> createState() => _State();
+}
+
+class _State extends State<InitialScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
